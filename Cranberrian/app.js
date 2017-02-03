909857978a8d33ed5d4ad0cbcfdf20c63b5804cd
ma223im <ma223im@student.lnu.se>
2017-02-03 11:06:17 +0100

    Week 5: Upload 1
    Log, HTML and java

--- Cranberrian/app.js
+++ Cranberrian/app.js
@@ -1,5 +1,5 @@
 /**
  * Created by mikaelandersson on 2017-02-02.
  */
-var quizGame = require("./quiz.js");
-new quizGame();
+var berrianJS = require("./berrianJS.js");
+new berrianJS();
--- Cranberrian/berrianJS.js
+++ Cranberrian/berrianJS.js
@@ -5,5 +5,5 @@
  * Created by mikaelandersson on 30/09/16.
  */
 module.exports = function () {
-   
+
 }
