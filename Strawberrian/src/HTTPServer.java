451431990dac9cbff4836e4ea0307caff8f84e22
Mikael Andersson <Mikael Andersson>
2017-02-09 13:07:06 +0100

    Week 6, Thursday commit 1

--- Strawberrian/src/HTTPServer.java
+++ Strawberrian/src/HTTPServer.java
@@ -1,57 +1,82 @@
 /**
  * Created by mikaelandersson on 2017-02-02.
  */
-import java.io.BufferedInputStream;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.Headers;
 
 public class HTTPServer {
 
+
+    private static String webFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\Cranberrian";
+    private static String rootFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\root";
+
+    private static  File fileIndex = new File (webFolder+"\\index.html");
+
+    private static int port = 8080;
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/info", new InfoHandler());
-        server.createContext("/get", new GetHandler());
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new GetHandler());
+        server.createContext("/static", new StaticFileServer(webFolder));
         server.setExecutor(null); // creates a default executor
         server.start();
-    }
-
-    static class InfoHandler implements HttpHandler {
-        public void handle(HttpExchange t) throws IOException {
-            String response = "Use /get to download a PDF";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
     }
 
     static class GetHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
 
-            // add the required response header for a PDF file
             Headers h = t.getResponseHeaders();
-            h.add("Content-Type", "application/pdf");
+            h.add("Content-Type", "text/html");
 
-            // a PDF (you provide your own!)
-            File file = new File ("/Users/mikaelandersson/Documents/Skolarbeten/Projekt_Datorteknik/ProjektArbete/Documentation/Projekt_ma223im_cj222jb.pdf");
-            byte [] bytearray  = new byte [(int)file.length()];
-            FileInputStream fis = new FileInputStream(file);
+            File[] fileDir = new File (rootFolder).listFiles();
+            showFiles(fileDir,webFolder);
+
+
+            byte [] bytearray  = new byte [(int)fileIndex.length()];
+            FileInputStream fis = new FileInputStream(fileIndex);
             BufferedInputStream bis = new BufferedInputStream(fis);
             bis.read(bytearray, 0, bytearray.length);
 
-            // ok, we are ready to send the response.
-            t.sendResponseHeaders(200, file.length());
+            t.sendResponseHeaders(200, fileIndex.length());
             OutputStream os = t.getResponseBody();
             os.write(bytearray,0,bytearray.length);
             os.close();
         }
     }
+    public static void showFiles(File[] files, String filename){
+        //Shows every file and folder of requested address as a HTML index list.
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(filename+"/index.html","UTF-8" );
+
+          writer.println("<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"/static/>\"> </head> <body><h1>");
+//            writer.println("<HTML><html><head><script src=\"/static\"></script></head> <body><h1>");
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    writer.println(("<li class=\"dir\">"  + file.getName()+"</li>"));
+                    showFiles(file.listFiles(), filename); // Calls same method again.
+                } else {
+                    writer.println("<li class=\"file\">"  + file.getName()+"</li>");
+                }
+            }
+            writer.println("</h1></body></html>");
+            writer.close();
+        } catch (FileNotFoundException e) {
+
+
+        } catch (UnsupportedEncodingException e) {
+
+        }
+    }
+
 }
---
+++ Strawberrian/src/StaticFileServer.java
@@ -0,0 +1,49 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by Mikael Andersson on 2017-02-08.
+ */
+
+@SuppressWarnings("restriction")
+public class StaticFileServer implements HttpHandler {
+
+
+    private static String webFolder;
+    public StaticFileServer(String webFolder) {
+        this.webFolder = webFolder;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String fileId = exchange.getRequestURI().getPath();
+        File file =  new File (webFolder+"\\style.css");
+        System.out.println("hej");
+        if (file == null) {
+            String response = "Error 404 File not found.";
+            exchange.sendResponseHeaders(404, response.length());
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.flush();
+            output.close();
+        } else {
+            exchange.sendResponseHeaders(200, 0);
+            OutputStream output = exchange.getResponseBody();
+            FileInputStream fs = new FileInputStream(file);
+            final byte[] buffer = new byte[0x10000];
+            int count = 0;
+            while ((count = fs.read(buffer)) >= 0) {
+                output.write(buffer, 0, count);
+            }
+            output.flush();
+            output.close();
+            fs.close();
+        }
+    }
+
+}
