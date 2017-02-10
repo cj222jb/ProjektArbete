e5934ceaf0a8e1851a397f39b67d8f6042e0c3f1
Mikael Andersson <Mikael Andersson>
2017-02-10 16:43:46 +0100

    Week 6, Friday commit 3

--- Cranberrian/scriptBerrian.js
+++ Cranberrian/scriptBerrian.js
@@ -1,3 +1,19 @@
 
 
-    console.log("hejsan")
+function addDirListener() {
+    
+}
+function addFileListener() {
+
+    var files = document.getElementById("Files").childNodes;
+    NodeList.prototype.forEach = Array.prototype.forEach
+    files.forEach(function(file){
+        file.addEventListener("click",function () {
+            console.log(file.firstChild.innerHTML);
+        });
+        file.addEventListener("right-click",function () {
+            console.log(file.firstChild.innerHTML+" Heeeeeej");
+        });
+    });
+}
+addFileListener();
--- Strawberrian/src/HTTPServer.java
+++ Strawberrian/src/HTTPServer.java
@@ -6,7 +6,6 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,16 +21,16 @@ import java.io.OutputStream;
 public class HTTPServer {
 
     /*Mikkes stationära*/
-//    private static String webFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\Cranberrian";
-//    private static String rootFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\root";
+    private static String webFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\Cranberrian";
+    private static String rootFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\root";
 //
 /*Mikkes laptop*/
 //    private static String webFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\Cranberrian";
 //    private static String rootFolder = "C:\\Users\\Mikael Andersson\\Documents\\Projects\\ProjektArbete\\root";
 
 /*Raspberry*/
-    private static String webFolder = "/home/Gooseberrian/ProjektArbete/Cranberrian";
-    private static String rootFolder = "/home/Gooseberrian/ProjektArbete/root";
+//    private static String webFolder = "/home/Gooseberrian/ProjektArbete/Cranberrian";
+//    private static String rootFolder = "/home/Gooseberrian/ProjektArbete/root";
 
     private static  File fileIndex = new File (webFolder+"/index.html");
 
@@ -39,14 +38,14 @@ public class HTTPServer {
     public static void main(String[] args) throws Exception {
         System.out.println("Up!");
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", new GetHandler());
+        server.createContext("/", new IndexHandler());
         server.createContext("/js", new StaticFileServer(webFolder, "/scriptBerrian.js"));
         server.createContext("/css", new StaticFileServer(webFolder, "/fancyBerrian.css"));
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class GetHandler implements HttpHandler {
+    static class IndexHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
 
             Headers h = t.getResponseHeaders();
@@ -70,8 +69,11 @@ public class HTTPServer {
     private static void addFileDirToHTML(File[] fileArr) throws IOException {
 
         Document doc = Jsoup.parse(fileIndex,"UTF-8","");
-        Element content = doc.getElementById("Content");
-        content.empty();
+        Element dirContent = doc.getElementById("Folders");
+
+        Element fileContent = doc.getElementById("Files");
+        dirContent.empty();
+        fileContent.empty();
         int dirCounter = 0, fileCounter = 0;
         for (File file : fileArr) {
             if (file.isDirectory()) {
@@ -80,17 +82,20 @@ public class HTTPServer {
                 aTag.append(file.getName());
                 liTag.appendChild(aTag);
                 liTag.attr("id","Dir"+dirCounter++);
-                content.appendChild(liTag);
+                liTag.attr("class", "Dir");
+                dirContent.appendChild(liTag);
             }
         }
+
         for (File file : fileArr) {
             if (!file.isDirectory()) {
                 Element liTag = doc.createElement("li");
                 Element aTag = doc.createElement("a");
                 aTag.append(file.getName());
                 liTag.appendChild(aTag);
-                liTag.attr("id","File"+dirCounter++);
-                content.appendChild(liTag);
+                liTag.attr("id","File"+fileCounter++);
+                liTag.attr("class","File");
+                fileContent.appendChild(liTag);
             }
         }
         PrintWriter out = new PrintWriter(new FileWriter(fileIndex));
