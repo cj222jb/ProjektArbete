ae8dd0fe769e1932a0a4f92a221cc0897538905d
cj222jb <cj222jb@student.lnu.se>
2017-03-10 14:20:19 +0100

    Vecka 10 Dag 5 1.2

--- Strawberrian/src/Authentication.java
+++ Strawberrian/src/Authentication.java
@@ -41,7 +41,7 @@ private static Statement stmt = null;
         String root = "";
         try{
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:users.db");
+            c = DriverManager.getConnection("jdbc:sqlite:/home/Gooseberrian/ProjektArbete/Strawberrian/users.db");
             c.setAutoCommit(false);
             System.out.println("Successfully opened DB");
 
