e8f8d08ede7c216502313a4c7caa51fdb04cf274
cj222jb <cj222jb@student.lnu.se>
2017-03-12 11:28:12 +0100

    rebuild

--- Strawberrian/src/Main.java
+++ Strawberrian/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Authentication authenticate = new Authentication();
 
-        HTTPServer webBerrian = new HTTPServer("/home/Gooseberrian/ProjektArbete/Cranberrian","/home/root/media/", 8080);
+        HTTPServer webBerrian = new HTTPServer("/home/Gooseberrian/ProjektArbete/Cranberrian","/home/Goosberrian/root/media/", 8080);
 
 
 //        HTTPServer webBerrian = new HTTPServer( "C:\\Users\\carl\\Documents\\GitHub\\2dt301\\ProjektArbete\\Cranberrian\\","C:\\Users\\carl\\Documents\\GitHub\\2dt301\\ProjektArbete\\root\\",8081);
